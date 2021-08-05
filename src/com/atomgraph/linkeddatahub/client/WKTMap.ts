1dc51a0cb2f0796a01070b95785056147b8aa9b5
Martynas <martynas@atomgraph.com>
2021-08-05 19:07:44 +0300

    Ported more code from main.js example

--- src/com/atomgraph/linkeddatahub/client/WKTMap.ts
+++ src/com/atomgraph/linkeddatahub/client/WKTMap.ts
@@ -1,13 +1,58 @@
-import { Tile } from 'ol/layer';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import WKT from 'ol/format/WKT';
+import {OSM, Vector as VectorSource} from 'ol/source';
+import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
 
 export class WKTMap
 {
 
-    private raster: Tile;
+    private format: WKT;
+    private raster: TileLayer;
+    private target: HTMLElement;
+    privaet view: View;
 
-    constructor(raster: Tile)
+    constructor(raster: TileLayer, target: HTMLElement, view: View)
     {
+        this.format = new WKT();
         this.raster = raster;
+        this.target = target;
+        this.view = view;
+    }
+
+    public render(wkt: string)
+    {
+        var feature = getFormat().readFeature(wkt, {
+          dataProjection: 'EPSG:4326',
+          featureProjection: 'EPSG:3857',
+        });
+
+        var vector = new VectorLayer({
+          source: new VectorSource({
+            features: [feature],
+          }),
+        });
+
+        var map = new Map({
+          layers: [raster, vector],
+          target: getTarget(),
+          view: getView()
+        });
+    }
+
+    public getFormat(): WKT
+    {
+        return format;
+    }
+
+    public getTarget(): HTMLElement
+    {
+        return target;
+    }
+
+    public getView(): View
+    {
+        return view;
     }
 
 }
