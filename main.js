679570bf5111f5b00697e55dde30caa44874fb71
Martynas <martynas@atomgraph.com>
2021-08-05 15:56:15 +0300

    Added WKT example

--- main.js
+++ main.js
@@ -1,17 +1,37 @@
 import 'ol/ol.css';
-import {Map, View} from 'ol';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import WKT from 'ol/format/WKT';
+import {OSM, Vector as VectorSource} from 'ol/source';
+import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
+
+const raster = new TileLayer({
+  source: new OSM(),
+});
+
+const wkt =
+  'POLYGON((10.689 -25.092, 34.595 ' +
+  '-20.170, 38.814 -35.639, 13.502 ' +
+  '-39.155, 10.689 -25.092))';
+
+const format = new WKT();
+
+const feature = format.readFeature(wkt, {
+  dataProjection: 'EPSG:4326',
+  featureProjection: 'EPSG:3857',
+});
+
+const vector = new VectorLayer({
+  source: new VectorSource({
+    features: [feature],
+  }),
+});
 
 const map = new Map({
+  layers: [raster, vector],
   target: 'map',
-  layers: [
-    new TileLayer({
-      source: new OSM()
-    })
-  ],
   view: new View({
-    center: [0, 0],
-    zoom: 2
-  })
+    center: [2952104.0199, -3277504.823],
+    zoom: 4,
+  }),
 });
