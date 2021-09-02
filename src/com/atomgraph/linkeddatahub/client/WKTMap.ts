c4f25a5cac5287aa6b292ad5c7585d79b2a3d650
Martynas <martynas@atomgraph.com>
2021-09-02 13:42:53 +0200

    Import Geometry

--- src/com/atomgraph/linkeddatahub/client/WKTMap.ts
+++ src/com/atomgraph/linkeddatahub/client/WKTMap.ts
@@ -1,7 +1,8 @@
 import Map from 'ol/Map';
 import View from 'ol/View';
 import WKT from 'ol/format/WKT';
-import Feature<Geometry> from 'ol/Feature';
+import Geometry from 'ol/geom/Geometry';
+import Feature from 'ol/Feature';
 import { OSM, Vector as VectorSource } from 'ol/source';
 import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer';
 
