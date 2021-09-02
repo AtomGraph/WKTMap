d7963a2b3919ef82b24e48c416be72134d8600e1
Martynas <martynas@atomgraph.com>
2021-09-02 13:45:48 +0200

    Allow null value for this.feature

--- src/com/atomgraph/linkeddatahub/client/WKTMap.ts
+++ src/com/atomgraph/linkeddatahub/client/WKTMap.ts
@@ -13,7 +13,7 @@ export class WKTMap
     private target: HTMLElement;
     private view: View;
     private raster: TileLayer<OSM>;
-    private feature: Feature<Geometry>;
+    private feature: Feature<Geometry> | null;
 
     constructor(target: HTMLElement, view: View, raster?: TileLayer<OSM>)
     {
