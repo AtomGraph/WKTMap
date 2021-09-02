c24c888d38f11b444edfed33e4faea7eabc5fbf1
Martynas <martynas@atomgraph.com>
2021-09-02 13:46:28 +0200

    Fixed getFeature() return type

--- src/com/atomgraph/linkeddatahub/client/WKTMap.ts
+++ src/com/atomgraph/linkeddatahub/client/WKTMap.ts
@@ -64,7 +64,7 @@ export class WKTMap
         return this.raster;
     }
 
-    public getFeature(): Feature<Geometry>
+    public getFeature(): Feature<Geometry> | null
     {
         return this.feature;
     }
